36e7340e6e07b4734101d37e5ebd8098275a590e
dhananjay.jani <dhananjay.jani@cygnet.one>
2025-04-21 14:16:40 +0530

    Implemented folder upload route

--- CloudNext/DTOs/UserFolder/FolderUploadDto.cs
+++ CloudNext/DTOs/UserFolder/FolderUploadDto.cs
@@ -2,6 +2,7 @@
 {
     public class FolderUploadDto
     {
+        public Guid UserId { get; set; }
         public string ParentFolderId { get; set; } = string.Empty;
         public IFormFile ZipFile { get; set; } = null!;
     }
